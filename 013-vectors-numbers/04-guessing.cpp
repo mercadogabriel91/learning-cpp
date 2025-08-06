7d829e183bf5677f1a327e455f071649407f25bb
GABRIEL EMILIANO MERCADO <mercado.gabriel91@hotmail.com>
2025-08-06 15:44:30 +0800

    feat(lcpp-009): page 104 exercises

--- 013-vectors-numbers/04-guessing.cpp
+++ 013-vectors-numbers/04-guessing.cpp
@@ -10,141 +10,75 @@
  * “Is the number you are thinking of less than 50?”). Your program
  * should be able to identify the number after asking no more than seven questions. Hint: Use the < and <= operators and the
  * if-else construct.
+ * 
+ * Had to change the approach to something called "binary search" to ensure that the number can be guessed in 7 questions or less.
+ * https://en.wikipedia.org/wiki/Binary_search 
  */
 
-int divide_number(int &value)
+bool is_valid_input(char input)
 {
-    return value / 2;
-}
-
-bool is_valid_input(char &input)
-{
-    return (input == 'y' || input == 'n');
-}
-
-void up_or_down(char &read_val, bool &go_up)
-{
-    if (read_val == 'y')
-    {
-        go_up = true;
-    }
-    else
-    {
-        go_up = false;
-    }
-}
-
-std::vector<std::vector<int>> subdivide_vector(std::vector<int> &original_vector)
-{
-    // Calculate the split point (e.g., in the middle)
-    size_t mid_point = original_vector.size() / 2;
-
-    std::vector<int> first_half;
-    std::vector<int> second_half;
-
-    // Copy elements to the first half
-    first_half.assign(original_vector.begin(), original_vector.begin() + (mid_point - 1));
-
-    // Copy elements to the second half
-    second_half.assign(original_vector.begin() + (mid_point - 1), original_vector.end());
-
-    // Print the sub-vectors
-    // std::cout << "First half: ";
-    // for (int num : first_half)
-    // {
-    //     std::cout << num << " ";
-    // }
-    // std::cout << std::endl;
-
-    // std::cout << "Second half: ";
-    // for (int num : second_half)
-    // {
-    //     std::cout << num << " ";
-    // }
-    // std::cout << std::endl;
-
-    return {first_half, second_half};
-}
-
-bool is_even(int num)
-{
-    return (num % 2 == 0);
-}
-
-void perfom_computation(bool &go_up, std::vector<int> &number_range, int &target_value)
-{
-    if (go_up)
-    {
-        // std::cout << "it's in the upper 50%" << '\n';
-        std::vector<std::vector<int>> vector_parts = subdivide_vector(number_range);
-        std::vector<int> upper_half = vector_parts[1];
-
-        // save the new array and midpoint
-        number_range = upper_half;
-        upper_half[(upper_half.size() / 2)];
-        if (is_even(upper_half.size()))
-        {
-            target_value = upper_half[(upper_half.size() / 2)];
-        }
-        else
-        {
-            target_value = upper_half[(upper_half.size() / 2) - 1];
-        }
-    }
-    else
-    {
-        // std::cout << "it's in the lower 50%" << '\n';
-        std::vector<std::vector<int>> vector_parts = subdivide_vector(number_range);
-        std::vector<int> lower_half = vector_parts[0];
-
-        // save the new array and midpoint
-        number_range = lower_half;
-        target_value = lower_half[(lower_half.size() / 2)];
-    }
+    return (input == 'y' || input == 'Y' || input == 'n' || input == 'N');
 }
 
 int main()
 {
-    std::vector<int> number_range;
-    int MAX_QUESTIONS = 7;
-    int target_value = 100;
-    bool go_up = false;
-    char read_val;
+    int low = 1;
+    int high = 100;
+    int guess;
+    char response;
+    int questions_asked = 0;
+    const int MAX_QUESTIONS = 7;
 
-    for (int n = 1; n < 101; n++)
+    std::cout << "Think of a number between 1 and 100, and I'll guess it in "
+              << MAX_QUESTIONS << " questions or less!\n\n";
+
+    while (low <= high && questions_asked < MAX_QUESTIONS)
     {
-        number_range.push_back(n);
-    }
+        // Calculate the middle point
+        guess = low + (high - low) / 2;
+        questions_asked++;
 
-    std::cout << "This is a number guessing game (must be between 1 and 100). " << '\n';
+        std::cout << "Question " << questions_asked << ": ";
+        std::cout << "Is your number " << guess << " or less? (y/n): ";
 
-    for (int i = 0; i < MAX_QUESTIONS; i++)
-    {
-        if (i == 0)
-        {
-            std::cout << "Is the number you're thinking about " << divide_number(target_value) << " or more? y/n " << '\n';
-        }
-        else
-        {
-            std::cout << "Is the number you're thinking about " << target_value << " or more? y/n " << '\n';
-        }
+        std::cin >> response;
 
-        std::cin >> read_val;
-
-        if (is_valid_input(read_val))
+        if (!is_valid_input(response))
         {
-            // does it go up or down?
-            up_or_down(read_val, go_up);
-            // make some math shit based on that
-            perfom_computation(go_up, number_range, target_value);
-        }
-        else
-        {
-            std::cout << "This ain't it chief" << '\n';
+            std::cout << "Please enter 'y' for yes or 'n' for no.\n";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            questions_asked--; // Don't count invalid questions
+            continue;
         }
+
+        if (response == 'y' || response == 'Y')
+        {
+            // The number is <= guess, so search the lower half (including guess)
+            high = guess;
+        }
+        else
+        {
+            // The number is > guess, so search the upper half
+            low = guess + 1;
+        }
+
+        // Check if we've found it
+        if (low == high)
+        {
+            std::cout << "\nI found it! Your number is " << low << "!\n";
+            std::cout << "It took me " << questions_asked << " questions.\n";
+            return 0;
+        }
+
+        std::cout << "Current range: " << low << " to " << high
+                  << " (remaining possibilities: " << (high - low + 1) << ")\n\n";
     }
+
+    // This should never happen with proper binary search
+    std::cout << "Something went wrong - couldn't find your number in "
+              << MAX_QUESTIONS << " questions.\n";
+    std::cout << "Final range: " << low << " to " << high << "\n";
 
     return 0;
 }
